2988c08f4729035c810eb38bad2080faff9d9eb4
gadakshi <138123587+gadakshi@users.noreply.github.com>
2023-07-24 16:20:01 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -4,6 +4,6 @@ public static void main(String args[])
 {
 System.out.println("hi , I am Akki");
 System.out.println("I like Playing");
-System.out.println(3+4);
+System.out.println("And I want to go home");
 }
 }
