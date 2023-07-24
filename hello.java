68865feb888e07f51ac18c567f55b747af8e115f
gadakshi <138123587+gadakshi@users.noreply.github.com>
2023-07-24 16:18:34 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -2,7 +2,8 @@ class hello
 {
 public static void main(String args[])
 {
-System.out.println("hi , sapient");
+System.out.println("hi , I am Akki");
+System.out.println("I like Playing");
 System.out.println(3+4);
 }
 }
